4ec55fc31c2c0f89ec157090ac4b443d2f205af4
UNITED-IMAGING\hao.luo01 <hao.luo01@united-imaging.com>
2022-04-20 20:34:40 +0800

    feat: jquery

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 $(function () {
+  $("#selector").on("changed.bs.select", function (event, clickedIndex, isSelected, previousValue) {
+    console.log('clickedIndex: ', clickedIndex, ' isSelected: ', isSelected, ' previousValue: ', previousValue);
+  });
 });
